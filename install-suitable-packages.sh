5b658649a94bb8144f512b6f22bce83425a096d1
Selivanov Pavel <selivan2@gmail.com>
2013-10-29 09:32:26 +0300

    install-suitable-packages.sh: added acl

--- install-suitable-packages.sh
+++ install-suitable-packages.sh
@@ -1,3 +1,3 @@
 if ! which apt-get; then echo "No apt-get :("; exit 1; fi
 apt-get update
-apt-get install mc screen htop iotop sysstat sysv-rc-conf ipcalc unp
+apt-get install mc screen htop iotop sysstat sysv-rc-conf ipcalc unpacl
