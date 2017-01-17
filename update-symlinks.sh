19f96789ec63c02fa4f31fc7f7ce280cd9d7d969
Pavel Selivanov <selivan.at.github@gmail.com>
2017-01-17 15:58:39 +0300

    Modified update-symlinks script

--- update-symlinks.sh
+++ update-symlinks.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 #set -x
 
-cd `dirname $0`/dotfiles
-pwd
+cd `dirname "$0"`
 
-#which git > /dev/null 2>&1 && git pull
+# Update repository if possible
+which git >/dev/null && echo "Updating dotfiles repository" && git pull
 
-find . -type f -print0 | xargs -0 -n1 | while read name; do
+find . -path './.*' \! -path './.git/*' -type f -print0 | xargs -0 -n1 | while read name; do
 	name="$name"
-	link="$HOME${name#.}"
+	target=`readlink -f "$name"`
+	# remove leading dot from path: ${x#.}
+	dotfile="$HOME${name#.}"
 	dir=`dirname "$link"`
-	if [ "$dir" != "$HOME" ]; then
-		mkdir -p "$dir"
-	fi
-	if [ -e "$link" -a ! -L "$link" ]; then
-		mv "$link" "$link".bak`date +%Y-%M-%d_%H-%M-%S`
-	elif [ -L "$link" ]; then
-		rm "$link"
-	fi
-	name_full=`readlink -f "$name"`
-	ln -sf "$name_full" "$link"
-   	ls -l "$link"
-done
 
+	# Backup dotfile if it is not a link
+	if [ -e "$dotfile" -a ! -L "$dotfile" ]; then
+		backup="$dotfile".backup-`date +%Y-%M-%d_%H-%M-%S`
+		if mv "$dotfile" "$backup" ; then
+			echo "Created backup: $backup"
+		else
+			echo "ERROR: failed to create backup: $backup"
+			exit 1
+		fi
+	fi
+
+	# Do nothing if link is already ok
+	current_link=`readlink -f "$dotfile"`
+	[ "$current_link" -eq "$target" ] && continue
+
+	# Create directory if necessary and update symlink
+	if mkdir -p "$dir" && ln -sf "$target" "$dotfile"; then
+		echo "Symlink updated:" `ls -l "$dotfile"`
+	else
+		echo "ERROR: failed to create symlink $target"
+		exit 1
+	fi
+done
