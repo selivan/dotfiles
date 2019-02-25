e3452bdce9a4431406964f6480dba74af22d3f9f
Selivanov Pavel <selivan.at.github@gmail.com>
2019-02-25 20:18:27 +0300

    Update update-symlinks.sh

--- update-symlinks.sh
+++ update-symlinks.sh
@@ -4,14 +4,14 @@
 cd `dirname "$0"`
 
 # Update repository if possible
-which git >/dev/null && echo "Updating dotfiles repository" && git pull
+#which git >/dev/null && echo "Updating dotfiles repository" && git pull
 
 find . -path './.*' \! -path './.git/*' -type f -print0 | xargs -0 -n1 | while read name; do
 	name="$name"
 	target=`readlink -f "$name"`
 	# remove leading dot from path: ${x#.}
 	dotfile="$HOME${name#.}"
-	dir=`dirname "$link"`
+	dir="$HOME"/`dirname "$name"`
 
 	# Backup dotfile if it is not a link
 	if [ -e "$dotfile" -a ! -L "$dotfile" ]; then
@@ -24,12 +24,20 @@ find . -path './.*' \! -path './.git/*' -type f -print0 | xargs -0 -n1 | while r
 		fi
 	fi
 
+	# Create directory if it does not exist
+	if mkdir -p "$dir"; then
+		true
+	else
+		echo "ERROR: failed to create directory $dir"
+		exit 1
+	fi
+
 	# Do nothing if link is already ok
 	current_link=`readlink -f "$dotfile"`
 	[ "$current_link" = "$target" ] && continue
 
 	# Create directory if necessary and update symlink
-	if mkdir -p "$dir" && ln -sf "$target" "$dotfile"; then
+	if ln -sf "$target" "$dotfile"; then
 		echo "Symlink updated:" `ls -l "$dotfile"`
 	else
 		echo "ERROR: failed to create symlink $target"
